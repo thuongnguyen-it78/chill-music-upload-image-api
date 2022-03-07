eb05e5bba744c7546d52537b46ff2b3725792c32
Andres Nguyen <thuongnguyen.it78@gmail.com>
2022-03-07 09:51:50 +0700

    Update code

--- src/services/image.service.js
+++ src/services/image.service.js
@@ -1,7 +1,10 @@
+import createHttpError from 'http-errors'
+import { BAD_REQUEST } from '../constants/httpStatusCode.constant'
+
 class ImageService {
   async uploadImage(data) {
     if (data.file) return { path: data.file.path }
-    throw createError(BAD_REQUEST, 'Nothing image to upload')
+    throw createHttpError(BAD_REQUEST, 'Nothing image to upload')
   }
 }
 
